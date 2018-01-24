8542f6256fcb620c6696fcc82c66ae76f563c4e1
asday <sara.and.zuka@gmail.com>
2018-01-24 13:59:31 +0000

    Added an implementation based off `os.urandom`.
    Cutting out the middleman of `uuid`.

--- __main__.py
+++ __main__.py
@@ -1,19 +1,27 @@
 from timeit import timeit
 
-from implementations.uuidgen import uuidgen
 from implementations.produce_amount_keys import produce_amount_keys
+from implementations.urandomgen import urandomgen
+from implementations.uuidgen import uuidgen
 
 
-# print('Timing 40k keys 10 times with produce_amount_keys, stdlib randint')
-# print(timeit(
-#     stmt='produce_amount_keys(40000, _randint=randint)',
-#     setup='from __main__ import produce_amount_keys; from random import randint',  # noqa
-#     number=10,
-# ))
+print('Timing 40k keys 10 times with produce_amount_keys, stdlib randint')
+print(timeit(
+    stmt='produce_amount_keys(40000, _randint=randint)',
+    setup='from __main__ import produce_amount_keys; from random import randint',  # noqa
+    number=10,
+))
 
 print('Timing 40k keys 10 times with uuidgen, stdlib randint')
 print(timeit(
     stmt='list(uuidgen(40000))',
     setup='from __main__ import uuidgen',
+    number=10,
+))
+
+print('Timing 40k keys 10 times with urandomgen, stdlib randint')
+print(timeit(
+    stmt='list(urandomgen(40000))',
+    setup='from __main__ import urandomgen',
     number=10,
 ))
---
+++ implementations/urandomgen.py
@@ -0,0 +1,38 @@
+from base64 import b64encode
+from math import ceil
+from os import urandom
+from random import choice, randint
+from string import ascii_uppercase, digits
+
+
+ALLOWED_CHARS = [char.encode() for char in ascii_uppercase + digits]
+
+
+def urandomgen(count):
+    generated = set()
+
+    while True:
+        if len(generated) == count:
+            return
+
+        # desired_length = randint(12, 20)
+        #
+        # # Faster than math.ceil
+        # urandom_bytes = urandom(((desired_length + 1) * 3) // 4)
+        #
+        # candidate = b64encode(urandom_bytes, b'//').upper()
+        #
+        # The above is rolled into one line to cut down on execution
+        # time stemming from locals() dictionary access.
+
+        candidate = b64encode(
+            urandom(((randint(12, 20) + 1) * 3) // 4),
+            b'//',
+        ).upper()
+
+        while b'/' in candidate:
+            candidate = candidate.replace(b'/', choice(ALLOWED_CHARS), 1)
+
+        if candidate not in generated:
+            generated.add(candidate)
+            yield candidate.decode()
